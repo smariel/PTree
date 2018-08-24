25214bf6b38b9f0545d49c161a87c432d5a75675
Sylvain Mariel <sylvain.mariel@otmax.fr>
2018-08-24 19:50:17 +0200

    Stats for fonctions & tags (#2)

--- js/obj.Stats.js
+++ js/obj.Stats.js
@@ -20,96 +20,199 @@ var Stats = function(item, tree, partList) {
 };
 
 
-// update the Canvas by displaying the stats of the saved item
+// update the Canvas according to the active tab
 Stats.prototype.update = function() {
-   // if there is no item, display a message
-   if(null === this.item || undefined === this.item) {
-      this.empty();
+   const chartType = $('.selected').data('type');
+   if('tree' === chartType) {
+      this.updateTree();
    }
-   // if there is an item, display two charts
-   else {
-      // display the title
-      $('.title').html(`${this.item.characs.name}`);
-
-      // display a "go to parent" button if needed
-      let parent = this.item.getParent();
-      if(null !== parent && !parent.isRoot()) {
-         $('.goToParent > button').attr('title', parent.characs.name).tooltip('fixTitle');
-         $('.goToParent').fadeIn(200);
-      }
-      else {
-         $('.goToParent').fadeOut(200);
-      }
-
-      // create two charts and fill them
-      let datasets = {typ:[], max:[]};
-      let labels = [];
-      let clickCallback = null;
-      var that = this;
-      let max = 0;
-
-      // if the item is a source
-      if(this.item.isSource()) {
-         // prepare data for the chart
-         for (let childID of this.item.childrenID) {
-            let child = this.tree.getItem(childID);
-            let valtyp = smartRound(child.getInputCurrent('typ'), 2);
-            let valmax = smartRound(child.getInputCurrent('max'), 2);
-            datasets.typ.push(valtyp);
-            datasets.max.push(valmax);
-            labels.push(child.characs.name);
-            if(valmax > max) max = valmax;
-            if(valtyp > max) max = valtyp;
-         }
-
-         // prepare the callback for the "click" event
-         clickCallback = function(event,elements) {
-            if(elements.length > 0) {
-               that.item = that.tree.getItem(that.item.childrenID[elements[0]._index]);
-               that.updateTreeSelection();
-               that.update();
-               // TODO : do something with the Chart.js error
-            }
-         };
-      }
-      // else if the item is a load
-      else if(this.item.isLoad()) {
-         // if the currents are in the partlist
-         if(this.item.isInPartlist()) {
-            // check each part of the partlist to get the charts data
-            this.partList.forEachPart(function(part){
-               // if the part is consuming on this load, add it to the chart
-               if(part.isConsuming(that.item)) {
-                  let valtyp = smartRound(part.getConsumption(that.item, 'typ'),2);
-                  let valmax = smartRound(part.getConsumption(that.item, 'max'),2);
-                  datasets.typ.push(valtyp);
-                  datasets.max.push(valmax);
-                  labels.push(part.characs.name);
-                  if(valmax > max) max = valmax;
-                  if(valtyp > max) max = valtyp;
-               }
-            });
-
-            // if there is no parts
-            if(0 === labels.length) {
-               this.empty('No part');
-               return;
-            }
-         }
-         // else if the current is Raw or in the Spreadsheet
-         else if(this.item.isRaw() || this.item.isSynced()) {
-            datasets.typ.push(this.item.characs.ityp);
-            datasets.max.push(this.item.characs.imax);
-            labels.push(this.item.isRaw()?'Raw current':'Spreadsheet');
-         }
-      }
-
-      // create two charts and fill them
-      this.plotChart('typ', datasets.typ, labels, max, clickCallback);
-      this.plotChart('max', datasets.max, labels, max, clickCallback);
+   else if('functions' === chartType) {
+      this.updateFunctions();
+   }
+   else if('tags' === chartType) {
+      this.updateTags();
    }
 };
 
+
+// update the Canvas by displaying the stats of the selected item
+Stats.prototype.updateTree = function() {
+   // if there is no item, display a message
+   if(null === this.item || undefined === this.item) {
+      this.empty();
+      return;
+   }
+
+   // display the title
+   $('.title').html(`${this.item.characs.name}`);
+
+   // display a "go to parent" button if needed
+   let parent = this.item.getParent();
+   if(null !== parent && !parent.isRoot()) {
+      $('.goToParent > button').attr('title', parent.characs.name).tooltip('fixTitle');
+      $('.goToParent').fadeIn(200);
+   }
+   else {
+      $('.goToParent').fadeOut(200);
+   }
+
+   // create two charts and fill them
+   let datasets = {typ:[], max:[]};
+   let labels = [];
+   let clickCallback = null;
+   var that = this;
+   let max = 0;
+
+   // if the item is a source
+   if(this.item.isSource()) {
+      // prepare data for the chart
+      for (let childID of this.item.childrenID) {
+         let child = this.tree.getItem(childID);
+         let valtyp = smartRound(child.getInputCurrent('typ'), 2);
+         let valmax = smartRound(child.getInputCurrent('max'), 2);
+         datasets.typ.push(valtyp);
+         datasets.max.push(valmax);
+         labels.push(child.characs.name);
+         if(valmax > max) max = valmax;
+         if(valtyp > max) max = valtyp;
+      }
+
+      // prepare the callback for the "click" event
+      clickCallback = function(event,elements) {
+         if(elements.length > 0) {
+            that.item = that.tree.getItem(that.item.childrenID[elements[0]._index]);
+            that.updateTreeSelection();
+            that.update();
+            // TODO : do something with the Chart.js error
+         }
+      };
+   }
+   // else if the item is a load
+   else if(this.item.isLoad()) {
+      // if the currents are in the partlist
+      if(this.item.isInPartlist()) {
+         // check each part of the partlist to get the charts data
+         this.partList.forEachPart(function(part){
+            // if the part is consuming on this load, add it to the chart
+            if(part.isConsuming(that.item)) {
+               let valtyp = smartRound(part.getConsumption(that.item, 'typ'),2);
+               let valmax = smartRound(part.getConsumption(that.item, 'max'),2);
+               datasets.typ.push(valtyp);
+               datasets.max.push(valmax);
+               labels.push(part.characs.name);
+               if(valmax > max) max = valmax;
+               if(valtyp > max) max = valtyp;
+            }
+         });
+
+         // if there is no parts
+         if(0 === labels.length) {
+            this.empty('No part');
+            return;
+         }
+      }
+      // else if the current is Raw or in the Spreadsheet
+      else if(this.item.isRaw() || this.item.isSynced()) {
+         datasets.typ.push(this.item.characs.ityp);
+         datasets.max.push(this.item.characs.imax);
+         labels.push(this.item.isRaw()?'Raw current':'Spreadsheet');
+      }
+   }
+
+   // create two charts and fill them
+   this.plotChart('typ', datasets.typ, labels, max, 'current (A)', clickCallback);
+   this.plotChart('max', datasets.max, labels, max, 'current (A)', clickCallback);
+};
+
+
+// update the Canvas by displaying the stats of the functions
+Stats.prototype.updateFunctions = function() {
+   let datasets  = {typ:[], max:[]};
+   let labels    = [];
+   let functions = {};
+   let i         = 0;
+   let max       = 0;
+   let tree      = this.tree;
+
+
+   // display the title
+   $('.title').html('Global Power');
+
+   // hide the "go to parent" button
+   $('.goToParent').fadeOut(200);
+
+
+   this.partList.forEachPart(function(part){
+      if('' === part.characs.function) return;
+      let func  = part.characs.function;
+      let power = part.getPower(tree);
+
+      if(undefined === functions[func]) {
+         functions[func] = i;
+         i++;
+         datasets.typ.push(power.typ);
+         datasets.max.push(power.max);
+         labels.push(func);
+      }
+      else {
+         datasets.typ[functions[func]] += power.typ;
+         datasets.max[functions[func]] += power.max;
+      }
+
+      if(power.typ > max) max = power.typ;
+      if(power.max > max) max = power.max;
+   });
+
+   // create two charts and fill them, without click callback
+   this.plotChart('typ', datasets.typ, labels, max, 'power (W)', null);
+   this.plotChart('max', datasets.max, labels, max, 'power (W)', null);
+};
+
+
+// update the Canvas by displaying the stats of the tags
+Stats.prototype.updateTags = function() {
+   let datasets  = {typ:[], max:[]};
+   let labels    = [];
+   let tags      = {};
+   let i         = 0;
+   let max       = 0;
+   let tree      = this.tree;
+
+
+   // display the title
+   $('.title').html('Global Power');
+
+   // hide the "go to parent" button
+   $('.goToParent').fadeOut(200);
+
+
+   this.partList.forEachPart(function(part){
+      if('' === part.characs.tags) return;
+      let power     = part.getPower(tree);
+      let part_tags = part.characs.tags.split(/[\s]/);
+
+      for(let tag of part_tags) {
+         if(undefined === tags[tag]) {
+            tags[tag] = i;
+            i++;
+            datasets.typ.push(power.typ);
+            datasets.max.push(power.max);
+            labels.push(tag);
+         }
+         else {
+            datasets.typ[tags[tag]] += power.typ;
+            datasets.max[tags[tag]] += power.max;
+         }
+
+         if(power.typ > max) max = power.typ;
+         if(power.max > max) max = power.max;
+      }
+   });
+
+   // create two charts and fill them, without click callback
+   this.plotChart('typ', datasets.typ, labels, max, 'power (W)', null);
+   this.plotChart('max', datasets.max, labels, max, 'power (W)', null);
+};
 
 // remove both canvas and print a default text
 Stats.prototype.empty = function(title = 'No selection') {
@@ -121,7 +224,7 @@ Stats.prototype.empty = function(title = 'No selection') {
 
 
 // Fill a canvas with a chart and the given data
-Stats.prototype.plotChart = function(typmax, dataset, labels, max, clickCallback) {
+Stats.prototype.plotChart = function(typmax, dataset, labels, max, unit, clickCallback) {
    // destroy any previous chart
    if(null !== this.charts[typmax]) {
       this.charts[typmax].destroy();
@@ -141,7 +244,7 @@ Stats.prototype.plotChart = function(typmax, dataset, labels, max, clickCallback
          title: {
             display: true,
             position: 'top',
-            text: `${('typ' == typmax) ? 'Typical' : 'Maximum'} current (A)`
+            text: `${('typ' == typmax) ? 'Typical' : 'Maximum'} ${unit}`
          },
          legend: {
             display: ('doughnut' === this.chartType),
@@ -252,5 +355,14 @@ Stats.prototype.listenEvents = function() {
          $('.normalize > button').attr('title', 'adjust').tooltip('fixTitle').tooltip('show');
       }
       that.update();
+   });
+
+   // click on a button of the top menu
+   $('.topmenu > a').click(function(){
+      if(!$(this).hasClass('selected')) {
+         $('.selected').removeClass('selected');
+         $(this).addClass('selected');
+         that.update();
+      }
    });
 };
