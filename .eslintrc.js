a489fc18edd130ae0f37aca3e88edc169363cdb9
Sylvain Mariel <sylvain.mariel@otmax.fr>
2022-03-06 14:10:32 +0100

    eslint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,40 +10,49 @@ module.exports = {
     "ecmaVersion": 2017
   },
   "plugins": [
-		"ava"
-	],
+    "ava"
+  ],
   "rules": {
     "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single", { "allowTemplateLiterals": true }],
-    "semi": ["error", "always" ],
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    "quotes": ["error", "single", {
+      "allowTemplateLiterals": true
+    }],
+    "semi": ["error", "always"],
+    "no-console": ["error", {
+      allow: ["warn", "error"]
+    }],
     "no-undef": ["warn"],
     "no-unused-vars": ["warn"],
     "ava/assertion-arguments": "error",
-		"ava/max-asserts": ["off", 5],
-		"ava/no-async-fn-without-await": "error",
-		"ava/no-cb-test": "off",
-		"ava/no-duplicate-modifiers": "error",
-		"ava/no-identical-title": "error",
-		"ava/no-ignored-test-files": "error",
-		"ava/no-import-test-files": "error",
-		"ava/no-invalid-end": "error",
-		"ava/no-nested-tests": "error",
-		"ava/no-only-test": "error",
-		"ava/no-skip-assert": "error",
-		"ava/no-skip-test": "error",
-		"ava/no-statement-after-end": "error",
-		"ava/no-todo-implementation": "error",
-		"ava/no-todo-test": "warn",
-		"ava/no-unknown-modifiers": "error",
-		"ava/prefer-async-await": "error",
-		"ava/prefer-power-assert": "off",
-		"ava/test-ended": "error",
-		"ava/test-title": ["error", "if-multiple"],
-		"ava/use-t-well": "error",
-		"ava/use-t": "error",
-		"ava/use-test": "error",
-		"ava/use-true-false": "error",
+    "ava/hooks-order": "error",
+    "ava/max-asserts": [
+      "off",
+      5
+    ],
+    "ava/no-async-fn-without-await": "error",
+    "ava/no-duplicate-modifiers": "error",
+    "ava/no-identical-title": "error",
+    "ava/no-ignored-test-files": "error",
+    "ava/no-import-test-files": "error",
+    "ava/no-incorrect-deep-equal": "error",
+    "ava/no-inline-assertions": "error",
+    "ava/no-nested-tests": "error",
+    "ava/no-only-test": "error",
+    "ava/no-skip-assert": "error",
+    "ava/no-skip-test": "error",
+    "ava/no-todo-implementation": "error",
+    "ava/no-todo-test": "warn",
+    "ava/no-unknown-modifiers": "error",
+    "ava/prefer-async-await": "error",
+    "ava/prefer-power-assert": "off",
+    "ava/prefer-t-regex": "error",
+    "ava/test-title": "error",
+    "ava/test-title-format": "off",
+    "ava/use-t": "error",
+    "ava/use-t-throws-async-well": "error",
+    "ava/use-t-well": "error",
+    "ava/use-test": "error",
+    "ava/use-true-false": "error"
   }
 };
